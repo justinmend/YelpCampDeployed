1806cfe59bfa990016210f2cafdfaf167361e12b
Justin Mendiguarin <justin.mendiguarin@gmail.com>
2017-06-25 11:26:09 +0000

    Refactored mongoose code, added seeds file, added comment model, added comment new/create, and styled show page

--- v3/app.js
+++ v3/app.js
@@ -2,44 +2,19 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
-    Campground = require("./models/campground");
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds");
 
-
+seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp_v3");
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-//SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//     name: "Granite Hill",
-//     image: "https://farm4.staticflickr.com/3659/3662521481_4a7bcce691.jpg",
-//     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite."
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         }else {
-//             console.log("Newly created campground: ");
-//             console.log(campground);
-//         }
-//     }
-    
-// );
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
@@ -51,6 +26,7 @@ app.get("/campgrounds", function(req, res) {
     });
 });
 
+// CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res) {
     //get data from form and add to campgrounds array
     var name = req.body.name;
@@ -72,17 +48,19 @@ app.post("/campgrounds", function(req, res) {
     });
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
     res.render("new.ejs");
 });
 
-// Show - shows more info about one campground
+// SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
             console.log(err);
         } else {
+            console.log(foundCampground);
             //render show template with that campground
             res.render("show", {campground: foundCampground});
         }
